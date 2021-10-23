9e648003a0d684430013a4308c1e87debf30263b
thiagogvasc <62908111+thiagogvasc@users.noreply.github.com>
2021-10-23 12:35:39 -0400

    Fix bug

--- pages/api/user/[id]/index.js
+++ pages/api/user/[id]/index.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
     // Get info about a single user from the database
     case 'GET':
       try {
-        const user = await UserModel.findOne({ Username: `${req.query.username}` })
+        const user = await UserModel.findOne(req.query.id)
         res.json(user)
       } catch (err) {
         res.status(500).json({ message: err.message })
