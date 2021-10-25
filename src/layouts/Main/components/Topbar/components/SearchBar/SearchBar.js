63adef22aa74180d6686d5ac859d8229e6826158
David Moore <davidmoore2014@fau.edu>
2021-10-24 23:20:28 -0400

    Minor change.

--- src/layouts/Main/components/Topbar/components/SearchBar/SearchBar.js
+++ src/layouts/Main/components/Topbar/components/SearchBar/SearchBar.js
@@ -15,6 +15,7 @@ export default function SearchBar() {
     <Paper
       component="form"
       sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400}}
+      data-aos="fade-up"
     >
 
       <InputBase 
