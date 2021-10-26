da820a49c34b1d8f4f8d083b2760fa2a3137ce25
thiagogvasc <62908111+thiagogvasc@users.noreply.github.com>
2021-10-25 22:04:51 -0400

    Update account schema

--- src/models/account.js
+++ src/models/account.js
@@ -3,7 +3,15 @@ import mongoose from 'mongoose'
 // Create a schema of the data to be stored in the database
 const accountSchema = new mongoose.Schema({
     user_id: { type: mongoose.Types.ObjectId },
-    username: {
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
         type: String,
         required: true,
         unique: true
