224f349f7767dd597f9cb1d957aa85baebe3ca5e
Laura Casals <lauracasals7@gmail.com>
2021-12-05 17:13:14 -0500

    Minor Update to Profile page
    Made the user's name display in the center and added styling to the bio/goal section to make each an individual card.

--- src/views/PortfolioPage/components/Features/Features.js
+++ src/views/PortfolioPage/components/Features/Features.js
@@ -8,131 +8,50 @@ import { CardHeader, IconButton } from '@mui/material';
 import CardContent from '@mui/material/CardContent';
 import EditIcon from '@mui/icons-material/Edit';
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
 import { useContext } from 'react'
 
 import axios from 'axios'
 
-//hard coded information for testing bio + goal cards
+//Hard coded values for the names of the Bio and Goal section
 const mock = {
   biography: {
     title: 'Biography',
-    items: [
-      {
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        
-      },
-      
-    ],
   },
   goals: {
     title: 'Goals',
-    items: [
-      {
-        title: 'Goal 1',
-      },
-      {
-        title: 'Goal 2',
-      },
-      {
-        title: 'Goal 3',
-      },
-      {
-        title: 'Goal 4',
-      },
-    ],
+    
   },
 };
 
 
 const Features = () => {
-
   const [portfolio, setPortfolio] = useState([])
   const [user, setUser] = useState([])
-  const router = useRouter()
-
   
 
-  useEffect(async () => {
-    try {
-      const userData = window.localStorage.getItem('user')
-      const jsonUserData = JSON.parse(userData)
+  
+useEffect(async () => {
+  try {
+    const userData = window.localStorage.getItem('user')
+    const jsonUserData = JSON.parse(userData)
 
-      console.log("Getting portfolio data")
-      const res = await axios.get(`/api/user/${jsonUserData._id}/portfolio`)
-      //const res = await axios.get(`/api/user/${router.query.id}/portfolio`)
-      console.log(res.data)
-      setPortfolio(res.data)
+    console.log("Getting portfolio data")
+    const res = await axios.get(`/api/user/${jsonUserData._id}/portfolio`)
+    //const res = await axios.get(`/api/user/${router.query.id}/portfolio`)
+    setPortfolio(res.data)
 
-      const userRes = await axios.get(`/api/user/${jsonUserData._id}`)
-      //const userRes = await axios.get(`/api/user/${router.query.id}`)
-      setUser(userRes.data)
+    const userRes = await axios.get(`/api/user/${jsonUserData._id}`)
+    //const userRes = await axios.get(`/api/user/${router.query.id}`)
+    setUser(userRes.data)
 
-    } catch (error) {
-      console.log(error)
-    }
+   } catch (error) {
+    console.log(error)
+  }
     
   }, [])
-
-  console.log(portfolio.biography)
-  console.log('biography')
-
-
-
-
-
-
-  const renderFeaturesBox = (title = '',) => (
-    //This box makes the card and adds a shadow when the cursor hovers over
-    <Box
-      component={Card}
-      variant={'outlined'}
-      bgcolor={'transparent'}
-      sx={{
-        cursor: 'pointer',
-        '&:hover': {
-          boxShadow: 2,
-        },
-      }}
-    >
-      <CardContent>
-      <CardHeader
-
-          //This is where I added the edit icon
-          //goal is to be able to click on the icon and edit the cards directly
-          //action={
-            //<IconButton href = "../../"onClick={() => console.log('edit', mock.biography.title )}>
-             // <EditIcon />
-              
-           // </IconButton> 
-             
-         // }
-          />
-        
-          <IconButton href ={'../../portfolio'}>
-            <EditIcon/>
-          </IconButton>
-        <Box
-          display={'flex'}
-          flexDirection={{ xs: 'column', sm: 'row' }}
-          flex={'1 1 100%'}
-          justifyContent={{ sm: 'space-between' }}
-          alignItems={{ sm: 'center' }}
-        >
-          <Typography fontWeight={700} sx={{ marginBottom: { xs: 1, sm: 0 } }}>
-            {title}
-          </Typography>
-        </Box>
-      </CardContent>
-    </Box>
-  );
-
   
   return (
 
-
-    
-    
     //This is where the biography card is
     <Box>
       <Grid container spacing={4}>
@@ -140,94 +59,128 @@ const Features = () => {
           <Box marginBottom={2}>
 
             {/* displays "Biography" to screen*/}
-            <Typography variant={'h6'} fontWeight={700} gutterBottom>
+            <Typography variant={'h5'} fontWeight={700} gutterBottom>
               {mock.biography.title}
               <IconButton href ={'../../portfolio/'}>
-            <EditIcon/>
-          </IconButton>
+                <EditIcon/>
+              </IconButton>
             </Typography>
-            
-
-          
           </Box>
-
+          {/* This is what displays the user's biography info from the database */}
           {portfolio.map((portfolio, index) => (
-            <Card variant="outlined" sx={{ minWidth: 275, margin: 1}}>
+          <Card 
+            variant="outlined" 
+            sx={{ 
+              minWidth: 275, margin: 1,
+              cursor: 'pointer',
+                '&:hover': {
+                boxShadow: 2,
+              },
+              }}>
               
             <CardContent>
-              
-              <Typography variant="h4" component="div">
-                {portfolio.biography}
+              <Typography variant="h6" component="div" fontWeight={700} >
+                {portfolio.biography + " "}
+                <IconButton href ={'../../portfolio/'+portfolio._id} size = {'small'}>
+                  <EditIcon/>
+                </IconButton>
               </Typography>
-              <IconButton href ={'../../portfolio/'+portfolio._id}>
-            <EditIcon/>
-          </IconButton>
             </CardContent>
-            
           </Card>
           
           ))}
-
-
-
         </Grid>
+
+
         {/* This is where the goals cards are*/}
         <Grid item xs={12} md={6}>
           <Box marginBottom={2}>
             
             {/* displays "Goals" to screen*/}
-            <Typography variant={'h6'} fontWeight={700} gutterBottom>
+            <Typography variant={'h5'} fontWeight={700} gutterBottom>
               {mock.goals.title}
               <IconButton href ={'../../portfolio/'}>
-            <EditIcon/>
-          </IconButton>
+                <EditIcon/>
+              </IconButton>
             </Typography>
           </Box>
 
+          {/* This is what displays the user's goals info from the database */}
           {portfolio.map((portfolio, index) => (
-            <Card variant="outlined" sx={{ minWidth: 275, margin: 1}}>
-              
-            <CardContent>
-              
-              <Typography variant="h4" component="div">
-                {"Goal 1 : " + portfolio.goal1}
-              </Typography>
-              <IconButton href ={'../../portfolio/'+portfolio._id}>
-            <EditIcon/>
-          </IconButton>
-            </CardContent>
-
-            <CardContent>
-              
-              <Typography variant="h4" component="div">
-                {"Goal 2 : " + portfolio.goal2}
-              </Typography>
-              <IconButton href ={'../../portfolio/'+portfolio._id}>
-            <EditIcon/>
-          </IconButton>
-            </CardContent> 
-
-           <CardContent>
-              
-              <Typography variant="h4" component="div">
-                {"Goal 3 : " + portfolio.goal3}
-              </Typography>
-              <IconButton href ={'../../portfolio/'+portfolio._id}>
-            <EditIcon/>
-          </IconButton>
-            </CardContent>  
-
-           <CardContent>
-              
-              <Typography variant="h4" component="div">
-                {"Goal 4: " + portfolio.goal4}
-              </Typography>
-              <IconButton href ={'../../portfolio/'+portfolio._id}>
-            <EditIcon/>
-          </IconButton>
-            </CardContent>                                 
+          <Box>
             
-          </Card>
+            <Card 
+              variant="outlined" 
+              sx={{ minWidth: 275, margin: 1,
+                cursor: 'pointer',
+                '&:hover': {
+                boxShadow: 2,
+              },
+              }}>
+             <CardContent>
+              <Typography variant="h6" component="div" fontWeight={700}>
+                {"Goal 1 : " + portfolio.goal1 + "  "}
+                <IconButton href ={'../../portfolio/'+portfolio._id} size = {'small'}>
+                  <EditIcon/>
+                </IconButton>
+              </Typography>
+             </CardContent>
+            </Card>
+            
+            <Card 
+              variant="outlined" 
+              sx={{ minWidth: 275, margin: 1,
+                cursor: 'pointer',
+                '&:hover': {
+                boxShadow: 2,
+              },
+              }}>
+             <CardContent>
+              <Typography variant="h6" component="div" fontWeight={700}>
+                {"Goal 2 : " + portfolio.goal2 + "  "}
+                <IconButton href ={'../../portfolio/'+portfolio._id} size = {'small'}>
+                  <EditIcon/>
+                </IconButton>
+              </Typography>
+             </CardContent> 
+            </Card>
+
+            <Card 
+              variant="outlined" 
+              sx={{ minWidth: 275, margin: 1,
+                cursor: 'pointer',
+                '&:hover': {
+                boxShadow: 2,
+              },
+              }}>
+            <CardContent>
+              <Typography variant="h6" component="div" fontWeight={700}>
+                {"Goal 3 : " + portfolio.goal3 + "  "}
+                <IconButton href ={'../../portfolio/'+portfolio._id} size = {'small'}>
+                  <EditIcon/>
+                </IconButton>
+              </Typography>
+             </CardContent>  
+            </Card>
+
+            <Card 
+              variant="outlined" 
+              sx={{ minWidth: 275, margin: 1,
+                cursor: 'pointer',
+                '&:hover': {
+                boxShadow: 2,
+              },
+              }}>
+            <CardContent>
+              <Typography variant="h6" component="div" fontWeight={700}>
+                {"Goal 4: " + portfolio.goal4 + "  "}
+                <IconButton href ={'../../portfolio/'+portfolio._id} size = {'small'}>
+                  <EditIcon/>
+                </IconButton>
+              </Typography>
+             </CardContent>  
+            </Card>                               
+          </Box>
           
           ))}
 
@@ -239,10 +192,3 @@ const Features = () => {
 
 
 export default Features;
-
-async function getPortfolio() {
-  const userData = JSON.parse(window.localStorage.getItem('user'))
-  const portfolio = await axios.get(`/api/user/${userData._id}/portfolio`)
-
-  return portfolio.data
-}
