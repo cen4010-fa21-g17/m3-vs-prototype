4976a5de29977542b3333c767b76fd72a44af164
Laura Casals <lauracasals7@gmail.com>
2021-12-06 17:01:23 -0500

    minor update
    updated images.formats

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     images: {
       domains: ['assets.vercel.com', 'localhost'],
-      formats: ['image/avif', 'image/webp', 'image/png', 'image/jpeg'],
+      formats: ['image/png', 'image/jpeg'],
     },
   }
