fbe53533434faffc7addac5badee376f1e5b47bd
ialbornoz2016 <89750086+ialbornoz2016@users.noreply.github.com>
2021-12-06 18:44:52 -0500

    analytics
    adjusting script for next.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -17,14 +17,22 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-        <script>{
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-MZ25KHT') 
-          }
-        </script>
+        <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <meta charSet="utf-8" />
           <link
             rel="shortcut icon"
@@ -65,10 +73,10 @@ export default class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet" />
         </Head>
         <body>
-        <noscript>
+        {/* <noscript>
             <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MZ25KHT"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>
-        </noscript>
+        </noscript> */}
           <Main />
           <NextScript />
         </body>
